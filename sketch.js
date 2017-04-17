4748ff18bcaa004c9832513561b3cbc6fae3a3c5
oscarjoker10 <sibayoscar@hotmail.com>
2017-04-17 09:09:58 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
-var HT = window.innerHeight;
-var WD = HT;
+var h = window.innerHeight;
+var w = h;
 var speedx,speedy;
-
+var x,y,H,W;
 function setup()
 {
- HT = HT - (HT%100);
- WD = HT;
-createCanvas(WD + 200, HT);
+ h = h - (h%100);
+ w = h;
+createCanvas(w + 200, h);
 background(0);
+ W=25;
+ H=25;
+ y=h/2;
+ x=w/2;
+ 
 
 }
 function draw()
@@ -20,16 +25,35 @@ text("version : 1",0,0);
 function ball()
 {
  color(225);
- ellipse(500,500,30.30);
+ fill(225);
+ ellipse(x,y,H,W);
+ if(x>w-W/2)
+ {
+ speedx=-speedx;
+ }
+ if(x<0+W/2)
+ {
+ speedx=-speedx;
+ }
+if(y>650-H/2)
+{
+speedy=-speedy;
+}
+ if(y<73+H/2)
+{
+speedy=-speedy;
+}
+ x=x+speedx;
+ y=y+speedy;
 }
 function ScoreBoard()
 {
  color(225);
  stroke(250);
- text("DONG"WD/2,800);
- line(0,72,WD,72);
- line(0,73,WD,73);
- line(0  ,750,WD+200,750);
- line(0  ,751,WD+200,751);
+ text("DONG"w/2,800);
+ line(0,72,w+200,72);
+ line(0,73,w+200,73);
+ line(0  ,750,w+200,750);
+ line(0  ,751,w+200,751);
 }
  
