35d771a160244256c743e3b3bce9d87ca1a2ae1f
oscarjoker10 <sibayoscar@hotmail.com>
2017-04-17 08:27:08 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 var HT = window.innerHeight;
 var WD = HT;
- var x,y,h,w,player1,player2,score=7,d;
+// var x,y,h,w,player1,player2,score=7,d;
 
- var speedx,speedy;
+ /*var speedx,speedy;
  //paddles
  
  var up1,down1,up2,down2,play;
  var paddlexL,paddleyL,paddlewL,paddlehL,paddlesL;
   var paddlexR,paddleyR,paddlewR,paddlehR,paddlesR;
   var r,g,b; var look;
-  var len1,len2;
+  var len1,len2;*/
 function setup()
 {
  HT = HT - (HT%100);
@@ -17,7 +17,7 @@ function setup()
 createCanvas(WD + 200, HT);
   frameRate(100);
  background(225);
- smooth();
+ 
 }
 function draw()
 {
