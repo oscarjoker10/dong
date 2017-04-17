ff1af3832d493c6490aaf205fd5146c734390950
oscarjoker10 <sibayoscar@hotmail.com>
2017-04-17 08:29:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,6 @@
 var HT = window.innerHeight;
 var WD = HT;
-// var x,y,h,w,player1,player2,score=7,d;
 
- /*var speedx,speedy;
- //paddles
- 
- var up1,down1,up2,down2,play;
- var paddlexL,paddleyL,paddlewL,paddlehL,paddlesL;
-  var paddlexR,paddleyR,paddlewR,paddlehR,paddlesR;
-  var r,g,b; var look;
-  var len1,len2;*/
 function setup()
 {
  HT = HT - (HT%100);
