a5aff0808b53fb4e435eca34ea834a1f217ff7e3
oscarjoker10 <sibayoscar@hotmail.com>
2017-04-17 08:53:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var HT = window.innerHeight;
 var WD = HT;
+var speedx,speedy;
 
 function setup()
 {
@@ -13,6 +14,21 @@ function draw()
 {
 text("version : 1",0,0);
  fill(225,0,0);
-ellipse(222,222,80.80);
+
+ ScoreBoard();
+}
+function ball()
+{
+ color(225);
+ ellipse(500,500,30.30);
+}
+function ScoreBoard()
+{
+ color(225);
+ stroke(250);
+ line(0,72,WD,72);
+ line(0,73,WD,73);
+ line(0  ,650,WD,650);
+ line(0  ,651,WD,651);
 }
  
